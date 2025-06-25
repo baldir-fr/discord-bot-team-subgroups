122cf5a37f6d2ddd347cde5eac1916da1620f723
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-06-26 01:21:31 +0200

    Initialize Deno Fresh server

---
+++ routes/api/interactions/index.ts
@@ -0,0 +1,11 @@
+import {Handlers} from "$fresh/server.ts";
+
+export const handler: Handlers<string | null> = {
+  async POST(req, _ctx) {
+    return new Response(`Le sous-groupe "Foobar" sera composé de:
+    - Foo Bar
+    - Baz Buzz
+    - Bizz Boos`);
+  },
+};
+
---
+++ routes/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+      <main>
+        <h1>discord-bot-team-subgroups</h1>
+        <h2>Usage</h2>
+        <h2>Installation</h2>
+        <h2>REST API Endpoints</h2>
+        <p>
+          Routes begins with <code>/api</code>
+        </p>
+        <ul>
+          <li><code>/interactions</code></li>
+        </ul>
+      </main>
+  );
+}
