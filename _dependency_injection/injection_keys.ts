dcd5485161acccf5cae85b805e99b53abd61199c
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-06-26 01:24:39 +0200

    Injectable Discord API with in-memory implementation

--- _dependency_injection/injection_keys.ts
+++ _dependency_injection/injection_keys.ts
@@ -1,8 +1,8 @@
 import {key} from 'npm:piqure';
-// import {DiscordApi} from "../lib/discord_api.ts";
+import {DiscordApi} from "../lib/discord_api.ts";
 import {PseudoRandom} from "../lib/PseudoRandom.ts";
 
 export const InjectKey = {
-  // DISCORD_API: key<DiscordApi>("DiscordApi"),
+  DISCORD_API: key<DiscordApi>("DiscordApi"),
   PSEUDO_RANDOM: key<PseudoRandom>("PseudoRandom.ts"),
 };
