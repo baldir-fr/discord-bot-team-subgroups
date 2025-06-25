9a97009eb5b811124ad40d84f41eb714c2e8f059
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-06-26 01:23:49 +0200

    Injectable Pseudo random number gnerator

---
+++ lib/PseudoRandom.ts
@@ -0,0 +1,19 @@
+import {Prng, randomSeeded} from "jsr:@std/random";
+
+export interface PseudoRandom {
+  generateNumber(): number;
+}
+
+export class SeededPseudoRandom implements PseudoRandom {
+  generate: Prng;
+  constructor(seed: bigint) {
+    this.generate = randomSeeded(seed);
+  }
+
+// 💡Remember doctest exists
+//   https://docs.deno.com/runtime/fundamentals/testing/#documentation-tests
+
+  generateNumber(): number {
+    return this.generate();
+  }
+}
---
+++ tests/seeded_pseudo_random.test.ts
@@ -0,0 +1,12 @@
+import {expect} from "jsr:@std/expect";
+import {SeededPseudoRandom} from "../lib/PseudoRandom.ts";
+
+const test = Deno.test;
+
+test("seeded pseudo random",()=>{
+    const prng = new SeededPseudoRandom(1n);
+    expect(prng.generate()).toBe(0.20176767697557807);
+    expect(prng.generate()).toBe(0.4911644416861236);
+    expect(prng.generate()).toBe(0.7924694607499987);
+    expect(prng.generate()).toBe(0.0690375417470932);
+})
